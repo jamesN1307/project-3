29f652166f56802384e96c2f4d5ac10e15387166
Eric Taylor <eric.taylor1012@gmail.com>
2022-05-26 22:42:00 -0700

    landing page

--- src/components/home.js
+++ src/components/home.js
@@ -1,14 +1,35 @@
 import React from 'react';
 
 const styles = {
-    navbarStyle: {
+    image: {
         width: '100%',
-        height: 'auto',
+        height: '100%',
         position: 'absolute',
         left: 0,
         top: 0,
-        zIndex: 1,
+        zIndex: -1,
     },
+    h1: {
+        marginTop: "5%",
+        fontSize: "80px",
+        fontFamily: "herculanum",
+        textShadow: "3px 3px 3px skyblue"
+    },
+    navBar: {
+        display: "flex",
+        flexDirection: "column",
+        width: "10rem",
+        height: "50rem",
+        marginLeft: "15vw",
+    },
+    buttons: {
+        borderRadius: ".1rem",
+        fontFamily: "herculanum",
+        fontWeight: "bold",
+        boxShadow: "2px 2px 2px black",
+        fontSize: "1rem"
+        //add Hover later
+    }
 
 };
 
@@ -17,17 +38,18 @@ export default function Welcome() {
   // We return all the JSX inside a parent element with a className of "container".
 
   return (
-    <div className="container">
-        <header>
-            <h1>Appa's Landing</h1>
-                <nav>
-                    <button>Login</button>
-                    <p>Don't have an account?</p>
-                    <button>Sign Up</button>
-                </nav>
-        </header>
+    <div className="container" >
         <div>
-            <img src="https://wallpapercave.com/wp/weaogjF.jpg" alt="Ang and epic background" />
+            <img style={styles.image} src="https://wallpapercave.com/wp/weaogjF.jpg" alt="Ang and epic background" />
+            <h1 style={styles.h1}>Appa's Landing</h1>
+                <nav style={styles.navBar}>
+                    <button style={styles.buttons}>Login</button>
+                    <br></br>
+                    {/* <p>Don't have an account?</p> */}
+                    <button style={styles.buttons}>Sign Up</button>
+                </nav>
+        </div>
+        <div>
         </div>
     </div>
   );
