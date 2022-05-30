505cbcaf0ff20bc0ca708160a157264df091b3aa
JnBanks <joku.banks@gmail.com>
2022-05-29 18:35:56 -0700

    initial server setup

---
+++ server/models/index.js
@@ -0,0 +1,19 @@
+// const User = require('./User');
+// const Level1 = require('./Level1');
+
+// User.hasOne(Level1, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE',
+// });
+
+// Level1.belongsTo(User, {
+//   foreignKey: 'user_id',
+// });
+
+// // Define a Driver as having many Cars, thus creating a foreign key in the `car` table
+// Level1.hasMany(User, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE',
+// });
+
+// module.exports = { User, Level1};
