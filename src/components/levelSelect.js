274bdd8869b1ae144eb6ca513f7565d1158ee86c
Nicholas Jones <nej5000@gmail.com>
2022-05-31 11:33:55 -0700

    began new styling for level select page

--- src/components/levelSelect.js
+++ src/components/levelSelect.js
@@ -10,28 +10,14 @@ const styles = {
         top: 0,
         zIndex: -1,
     },
-    h1: {
-        marginTop: "5%",
-        fontSize: "80px",
-        fontFamily: "herculanum",
-        textShadow: "3px 3px 3px skyblue"
-    },
-    navBar: {
-        display: "flex",
-        flexDirection: "column",
-        width: "10rem",
-        height: "50rem",
-        marginLeft: "15vw",
-    },
-    buttons: {
-        borderRadius: ".1rem",
-        fontFamily: "herculanum",
-        fontWeight: "bold",
-        boxShadow: "2px 2px 2px black",
-        fontSize: "1rem"
-        //add
-    }
 
+    pageHeader: {
+
+    },
+
+    buttonContainer: {
+
+    },
 };
 
 export default function Levels() {
@@ -40,17 +26,12 @@ export default function Levels() {
 
     return (
         <div className="container" >
+            <img style={styles.image} src="https://i.pinimg.com/originals/87/37/4c/87374c7fcdb4a9308621a23a4f9c9a69.png" alt="4 Nations Map" />
             <div>
-                <img style={styles.image} src="https://wallpapercave.com/wp/weaogjF.jpg" alt="Ang and epic background" />
-                <h1 style={styles.h1}>Appa's Landing</h1>
-                <nav style={styles.navBar}>
-                    <Link to="/login"><button style={styles.buttons}>Login</button></Link>
-                    <br></br>
-                    {/* <p>Don't have an account?</p> */}
-                    <Link to="/login"><button style={styles.buttons}>Signup</button></Link>
-                </nav>
-            </div>
-            <div>
+                <h1></h1>
+                <button></button>
+                <button></button>
+                <button></button>
             </div>
         </div>
     );
