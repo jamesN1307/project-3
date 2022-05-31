03583088730daaf7466bc2c2753c12fdc970141e
Nicholas Jones <nej5000@gmail.com>
2022-05-31 12:00:26 -0700

    added basic styling to level select page

--- src/components/levelSelect.js
+++ src/components/levelSelect.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from "react-router-dom";
 
 const styles = {
+    //background image
     image: {
         width: '100%',
         height: '100%',
@@ -9,15 +10,40 @@ const styles = {
         left: 0,
         top: 0,
         zIndex: -1,
+        filter: 'contrast(50%)',
+    },
+
+    //master div styling
+    mainContainer: {
+        display: 'flex',
+        height: '100%',
+        width: '100%',
     },
 
     pageHeader: {
-
+        backgroundColor: 'skyblue',
+        fontSize: '45pt',
+        textAlign: 'center',
+        padding: '0 15px 0',
+        borderRadius: '8px',
     },
 
     buttonContainer: {
-
+        display: 'flex',
+        height: '100%',
+        width: '100%',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
+
+    levelButton: {
+        backgroundColor: 'moccasin',
+        fontSize: '35pt',
+        borderRadius: '8px',
+        margin: '15px auto',
+        width: '20%',
+    }
 };
 
 export default function Levels() {
@@ -25,13 +51,13 @@ export default function Levels() {
     // We return all the JSX inside a parent element with a className of "container".
 
     return (
-        <div className="container" >
+        <div className="container" style={styles.mainContainer}>
             <img style={styles.image} src="https://i.pinimg.com/originals/87/37/4c/87374c7fcdb4a9308621a23a4f9c9a69.png" alt="4 Nations Map" />
-            <div>
-                <h1></h1>
-                <button></button>
-                <button></button>
-                <button></button>
+            <div style={styles.buttonContainer}>
+                <h1 style={styles.pageHeader}>Level Selection</h1>
+                <button style={styles.levelButton}>Level One</button>
+                <button style={styles.levelButton}>Level Two</button>
+                <button style={styles.levelButton}>Level Three</button>
             </div>
         </div>
     );
