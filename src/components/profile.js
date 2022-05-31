1024c7a75d3ea6b015e836684e74e9416db2e696
Nicholas Jones <nej5000@gmail.com>
2022-05-31 12:54:02 -0700

    reduced to only minimal styling

--- src/components/profile.js
+++ src/components/profile.js
@@ -10,27 +10,6 @@ const styles = {
         top: 0,
         zIndex: -1,
     },
-    h1: {
-        marginTop: "5%",
-        fontSize: "80px",
-        fontFamily: "herculanum",
-        textShadow: "3px 3px 3px skyblue"
-    },
-    navBar: {
-        display: "flex",
-        flexDirection: "column",
-        width: "10rem",
-        height: "50rem",
-        marginLeft: "15vw",
-    },
-    buttons: {
-        borderRadius: ".1rem",
-        fontFamily: "herculanum",
-        fontWeight: "bold",
-        boxShadow: "2px 2px 2px black",
-        fontSize: "1rem"
-        //add
-    }
 
 };
 
@@ -40,17 +19,9 @@ export default function Profile() {
 
     return (
         <div className="container" >
+            <img style={styles.image} src="https://wallpapercave.com/wp/weaogjF.jpg" alt="Ang and epic background" />
             <div>
-                <img style={styles.image} src="https://wallpapercave.com/wp/weaogjF.jpg" alt="Ang and epic background" />
-                <h1 style={styles.h1}>Appa's Landing</h1>
-                <nav style={styles.navBar}>
-                    <Link to="/login"><button style={styles.buttons}>Login</button></Link>
-                    <br></br>
-                    {/* <p>Don't have an account?</p> */}
-                    <Link to="/login"><button style={styles.buttons}>Signup</button></Link>
-                </nav>
-            </div>
-            <div>
+
             </div>
         </div>
     );
