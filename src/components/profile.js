699cf6ab27b4e96ed9d1b190aec0191e232c3d6d
Nicholas Jones <nej5000@gmail.com>
2022-05-31 14:06:12 -0700

    saving current work

--- src/components/profile.js
+++ src/components/profile.js
@@ -17,7 +17,11 @@ const styles = {
         flexDirection: 'column',
         width: '65%',
         height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
+    
+    //Items within left block
 
     //style right 'column' of page
     pageColumnRight: {
@@ -26,6 +30,7 @@ const styles = {
         width: '35%',
         height: '100%',
     },
+    //Items within right block
 };
 
 export default function Profile() {
@@ -36,35 +41,35 @@ export default function Profile() {
         <div className="container" style={styles.pageContainer}>
             {/*Left 'column' */}
             <section style={styles.pageColumnLeft}>
-                <h1></h1>
+                <h1>Profile</h1>
                 {/* username and scores */}
                 <article>
-                    <h2></h2>
+                    <h2>Username</h2>
                     {/*improvement - revisit, add loop over server elements to add sections for all level scores?}*/}
-                    <p></p>
-                    <p></p>
-                    <p></p>
-                    <p></p>
+                    <p>Level One - /score/</p>
+                    <p>Level Two - /score/</p>
+                    <p>Level Three - /score/</p>
+                    <p>Global Score - /score/</p>
                 </article>
 
                 {/* section with buttons to leaderboard and 'play game' buttons*/}
                 <section>
-                    <button></button>
+                    <button>Play Game</button>
                     <div>
-                        <button></button>
-                        <button></button>
+                        <button>Leaderboard - 1 Player</button>
+                        <button>Leaderboard - 2 Player</button>
                     </div>
                 </section>
             </section>
 
             {/*Right 'column' */}
             <section style={styles.pageColumnRight}>
-                <h1></h1>
+                <h1>Chat</h1>
                 {/*Chat container*/}
                 <div>
                     <section>
-                        <input></input>
-                        <button></button>
+                        <input type='text'></input>
+                        <input type='submit' value='New Comment'></input>
                     </section>
                     {/*sample elements*/}
                     <p></p>
